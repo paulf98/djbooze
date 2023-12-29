161bbfe15a3cff9b6f265682ebc73fc639bfd70d
Paul Faass <paulfaass@gmail.com>
2023-12-29 20:39:12 +0100

    feat: add google fonts and init empty hero component

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Hero />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxt/ui'],
+	modules: [
+		'@nuxt/ui',
+		[
+			'@nuxtjs/google-fonts',
+			{
+				families: {
+					Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+				},
+				display: 'swap',
+			},
+		],
+	],
+	colorMode: {
+		preference: 'dark',
+	},
 });
