1196c88312c553560a1e81adb62a19911098b092
Paul Faass <paulfaass@gmail.com>
2023-12-30 12:44:36 +0100

    feat: adjust the hero component and theme

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <Hero />
+    <Intro />
   </div>
 </template>
---
+++ components/intro.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex justify-center p-8">
+        INTRO
+    </div>
+</template>
