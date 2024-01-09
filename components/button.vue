af68f9f6dfe8eaaed1e4906b7350dd7741939b98
Paul Faass <paulfaass@gmail.com>
2024-01-09 17:40:59 +0100

    feat: add basic button component

---
+++ components/button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="text-white bg-red-600 px-4 py-2 rounded-sm">
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+
+const props = withDefaults(defineProps<{
+    primary: boolean
+}>(), {
+    primary: true
+})
+
+</script>
--- components/hero.vue
+++ components/hero.vue
@@ -4,12 +4,12 @@ import type { _primary } from '#tailwind-config/theme/accentColor';
     <div class="w-full bg-black flex flex-row justify-around gap-8 p-8">
         <div class="flex flex-col items-start justify-center gap-8">
             <NuxtImg src="djbooze-dark.png" alt="DJ Booze Logo" class="max-w-80" />
-            <p class="dark:text-white text-xl">
-                Get the party started with DJ Booze.
+            <p class="dark:text-white text-3xl font-thin tracking-wider">
+                Ein DJ für alle Fälle
             </p>
-            <UButton>
+            <Button>
                 Jetzt Anfrage senden
-            </UButton>
+            </Button>
         </div>
         <NuxtImg src="hero-image.jpeg" alt="DJ Booze" style="border-radius: 79% 21% 25% 75% / 30% 42% 58% 70% ;"
             class="max-w-80" />
