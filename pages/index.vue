0db28ecbae459cbf086000923e9315b2740f89c8
Paul Faass <paulfaass@gmail.com>
2024-01-10 21:47:21 +0100

    chore(seo): add basic seo

--- pages/index.vue
+++ pages/index.vue
@@ -6,3 +6,20 @@
     <Contact />
   </main>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'DJ Booze',
+  meta: [
+    { name: 'description', content: 'DJ Booze legt in Pirmasens und Umgebung auf. Egal ob Club, Hochzeit oder Event, DJ Booze hat immer die richtigen Tracks dabei.' }
+  ],
+})
+
+useSeoMeta({
+  title: 'DJ Booze',
+  ogTitle: 'DJ Booze',
+  description: 'DJ Booze legt in Pirmasens und Umgebung auf. Egal ob Club, Hochzeit oder Event, DJ Booze hat immer die richtigen Tracks dabei.',
+  ogDescription: 'DJ Booze legt in Pirmasens und Umgebung auf. Egal ob Club, Hochzeit oder Event, DJ Booze hat immer die richtigen Tracks dabei.',
+  ogImage: 'https://www.djbooze.de/_vercel/image?url=/djbooze-dark.png&w=1536&q=100',
+})
+</script>
