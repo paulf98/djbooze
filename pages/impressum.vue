ff09a889c4ed08b4c826e8e5a58ac3b00c941006
Paul Faass <paulfaass@gmail.com>
2024-01-10 18:11:23 +0100

    feat: add imprint

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        Impressum
+    <div class="container max-w-5xl py-16 md:py-32 px-8 prose text-white">
+        <h1 class="text-white">IMPRESSUM</h1>
+        <h2 class="text-white">WEBSEITENBETREIBER</h2>
+        <p>Paul Faass</p>
+
+        <h2 class="text-white">INFORMATIONEN ÜBER DAS UNTERNEHMEN</h2>
+        <p>Einzelunternehmer <br>
+            Hauptstraße 31 <br>
+            Lemberg , 66969 <br>
+            paulfaass@gmail.com <br>
+            01751159748
+        </p>
+        <h2 class="text-white">INFORMATIONEN ÜBER DIE UNTERNEHMENSREGISTRIERUNG</h2>
+        <p>35/039/42289</p>
     </div>
 </template>
