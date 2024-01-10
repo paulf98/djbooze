0afc63a7ae3bb1a734d218d63b77ef86cc85df62
Paul Faass <paulfaass@gmail.com>
2024-01-10 20:37:03 +0100

    feat: add data protection

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="container max-w-5xl py-16 md:py-32 px-8 prose text-white">
-        <h1 class="text-white">IMPRESSUM</h1>
-        <h2 class="text-white">WEBSEITENBETREIBER</h2>
+    <div class="container mx-auto py-16 md:py-32 px-8 prose text-white">
+        <h1>IMPRESSUM</h1>
+        <h2>WEBSEITENBETREIBER</h2>
         <p>Paul Faass</p>
 
-        <h2 class="text-white">INFORMATIONEN ÜBER DAS UNTERNEHMEN</h2>
+        <h2>INFORMATIONEN ÜBER DAS UNTERNEHMEN</h2>
         <p>Einzelunternehmer <br>
             Hauptstraße 31 <br>
             Lemberg , 66969 <br>
             paulfaass@gmail.com <br>
             01751159748
         </p>
-        <h2 class="text-white">INFORMATIONEN ÜBER DIE UNTERNEHMENSREGISTRIERUNG</h2>
+        <h2>INFORMATIONEN ÜBER DIE UNTERNEHMENSREGISTRIERUNG</h2>
         <p>35/039/42289</p>
     </div>
 </template>
+
+<style scoped>
+h1,
+h2 {
+    color: white;
+}
+</style>
