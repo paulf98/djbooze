c686f401cd568e3d18e4c5469efd3f218131a4a0
Paul Faass <paulfaass@gmail.com>
2024-01-10 17:55:13 +0100

    feat: add basic contact form

--- components/contact.vue
+++ components/contact.vue
@@ -3,31 +3,95 @@
         <div class="container max-w-5xl mx-auto flex flex-col md:flex-row justify-between items-center gap-16">
             <div class="flex flex-col">
                 <div class="container flex flex-col items-center max-w-5xl mx-auto mb-8">
-                    <Heading text="Contact"></Heading>
+                    <Heading text="Kontakt"></Heading>
                 </div>
-                <p class="max-w-md text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum,
-                    eos explicabo
-                    doloremque amet,
-                    assumenda possimus molestiae est a culpa quod modi autem nulla reprehenderit dolorem. Expedita ullam
-                    nemo quia!</p>
+                <p class="max-w-md text-justify">
+                    Du möchtest DJ Booze für deine Veranstaltung buchen? Dann schreib mir einfach eine Nachricht über das
+                    Kontaktformular. Alternativ freue ich mich auch über eine Nachricht auf Instagram.
+                </p>
             </div>
-            <form action="" class="w-full flex-grow">
+            <UForm :schema="schema" :state="state" :validate="validate" class="w-full flex-grow" @submit="submit">
                 <div class="container flex flex-col items-stretch gap-4 max-w-xl mx-auto py-8">
-                    <div class="flex flex-col gap-2">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" id="name" class="p-2 rounded-md" />
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="p-2 rounded-md" />
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <label for="message">Message</label>
-                        <textarea name="message" id="message" cols="30" rows="6" class="p-2 rounded-md"></textarea>
-                    </div>
-                    <Button>Send</Button>
+                    <UFormGroup label="Name" name="name" eager-validation>
+                        <UInput v-model="state.name" placeholder="Name" variant="outline" color="green" />
+                    </UFormGroup>
+                    <UFormGroup label="Email" name="email" eager-validation>
+                        <UInput v-model="state.email" placeholder="Email" variant="outline" color="green" />
+                    </UFormGroup>
+                    <UFormGroup label="Nachricht" name="message" eager-validation>
+                        <UTextarea v-model="state.message" placeholder="Nachricht ..." variant="outline" color="green" />
+                    </UFormGroup>
+                    <Button type="submit"
+                        :disabled="!state.name || !state.email || !state.message || !token">Senden</Button>
+                    <NuxtTurnstile v-model="token" :options="{ theme: 'dark' }" class="self-center" />
                 </div>
-            </form>
+            </UForm>
         </div>
     </section>
 </template>
+
+<script setup lang="ts">
+import { z } from 'zod'
+import type { FormError } from '#ui/types'
+
+
+const mail = useMail()
+const toast = useToast()
+
+const token = ref<string | undefined>(undefined)
+
+const validate = (state: any): FormError[] => {
+    const errors = []
+    if (!state.name) errors.push({ path: 'name', message: 'Name ist ein Pflichtfeld' })
+    if (!state.email) errors.push({ path: 'email', message: 'Email ist ein Pflichtfeld' })
+    if (!state.message) errors.push({ path: 'message', message: 'Nachricht ist ein Pflichtfeld' })
+    return errors
+}
+
+function submit() {
+    if (!mail || !token) {
+        alert('Ihre Anmeldung konnte nicht versendet werden.')
+        return
+    }
+
+    mail.send({
+        from: state.email,
+        subject: `Anfrage an DJ Booze von ${state.name}`,
+        text: `
+        Es gibt eine neue Anfrage an DJ Booze von ${state.name}.
+
+        E-Mail-Adresse: ${state.email}
+
+        Nachricht:
+        ${state.message}
+        `,
+    }).then(() => {
+        toast.add({
+            title: 'Anfrage versendet',
+            description: 'Ihre Anfrage wurde erfolgreich versendet.',
+            color: 'green',
+            timeout: 4000,
+        })
+
+    }).catch(() => {
+        alert('Ihre Anfrage konnte nicht versendet werden.')
+    })
+
+    // Reset form
+    Object.assign(state, defaultState)
+}
+
+const schema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    message: z.string().min(10),
+})
+
+const defaultState = {
+    name: "",
+    email: "",
+    message: "",
+}
+
+const state = reactive(defaultState)
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
 	modules: [
 		'@nuxt/ui',
 		'@nuxt/image',
+		'@nuxtjs/turnstile',
 		[
 			'@nuxtjs/google-fonts',
 			{
@@ -13,8 +14,39 @@ export default defineNuxtConfig({
 				display: 'swap',
 			},
 		],
+		[
+			'nuxt-mail',
+			{
+				message: {
+					to: process.env.EMAIL_APP_EMAIL,
+				},
+				smtp: {
+					host: process.env.EMAIL_APP_HOST,
+					port: 587,
+					auth: {
+						user: process.env.EMAIL_APP_EMAIL,
+						pass: process.env.EMAIL_APP_PASSWORD,
+					},
+				},
+			},
+		],
 	],
+	ui: {
+		// @ts-expect-error Somehow the typing for this is wrong
+		notifications: {
+			// Show toasts at the top right of the screen
+			position: 'top-0 bottom-auto',
+		},
+	},
 	colorMode: {
 		preference: 'dark',
+	},
+	turnstile: {
+		siteKey: process.env.TURNSTILE_SITE_KEY,
+	},
+	runtimeConfig: {
+		turnstile: {
+			secretKey: process.env.TURNSTILE_SECRET_KEY,
+		},
 	},
 });
