8e774578523f5bc1441fd2211ec0969d7f4ba30b
Paul Faass <paulfaass@gmail.com>
2024-01-11 08:22:01 +0100

    fix: increase heading backdrop opacity

--- components/heading.vue
+++ components/heading.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-start mb-4 max-w-full">
-        <div class="text-8xl sm:text-9xl -mb-20 opacity-10 text-red-600 z-0 shrink">{{ props.text }}</div>
+        <div class="text-8xl sm:text-9xl -mb-20 opacity-20 text-red-600 z-0 shrink">{{ props.text }}</div>
         <h1 class="text-4xl font-bold text-center ml-8 text-red-600 z-10 tracking-wider">
             {{ props.text }}
         </h1>
@@ -15,4 +15,4 @@ const props = withDefaults(defineProps<{
 }>(), {
     text: 'Heading'
 })
-</script>
+</script>   
