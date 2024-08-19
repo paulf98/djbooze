dd944d47be665df0f865c6889463e2a8f90f6747
Paul Faass <paulfaass@gmail.com>
2024-08-19 19:13:37 +0200

    fix: add missing script after turnstile was upgraded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,37 +2,30 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 
-	modules: [
-		'@nuxt/ui',
-		'@nuxt/image',
-		'@nuxtjs/turnstile',
-		[
-			'@nuxtjs/google-fonts',
-			{
-				families: {
-					Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-				},
-				display: 'swap',
-			},
-		],
-		[
-			'nuxt-mail',
-			{
-				message: {
-					to: process.env.EMAIL_APP_EMAIL,
-				},
-				smtp: {
-					host: process.env.EMAIL_APP_HOST,
-					port: 587,
-					auth: {
-						user: process.env.EMAIL_APP_EMAIL,
-						pass: process.env.EMAIL_APP_PASSWORD,
-					},
-				},
-			},
-		],
-		'@samk-dev/nuxt-vcalendar',
-	],
+	modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/turnstile', [
+        '@nuxtjs/google-fonts',
+        {
+            families: {
+                Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+            },
+            display: 'swap',
+        },
+    ], [
+        'nuxt-mail',
+        {
+            message: {
+                to: process.env.EMAIL_APP_EMAIL,
+            },
+            smtp: {
+                host: process.env.EMAIL_APP_HOST,
+                port: 587,
+                auth: {
+                    user: process.env.EMAIL_APP_EMAIL,
+                    pass: process.env.EMAIL_APP_PASSWORD,
+                },
+            },
+        },
+    ], '@samk-dev/nuxt-vcalendar', "@nuxt/scripts"],
 
 	ui: {
 		// @ts-expect-error Somehow the typing for this is wrong
