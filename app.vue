c997e9a606c13a849cfeeeb29da366a09b080b11
Paul Faass <paulfaass@gmail.com>
2024-08-19 20:53:30 +0200

    feat: install nuxt seo module

--- app.vue
+++ app.vue
@@ -24,8 +24,8 @@ useHead({
 useSeoMeta({
     title: 'DJ Booze',
     ogTitle: 'DJ Booze',
-    description: 'DJ Booze legt in Pirmasens und Umgebung auf. Egal ob Club, Hochzeit oder Event, DJ Booze hat immer die richtigen Tracks dabei.',
-    ogDescription: 'DJ Booze legt in Pirmasens und Umgebung auf. Egal ob Club, Hochzeit oder Event, DJ Booze hat immer die richtigen Tracks dabei.',
+    description: 'DJ Booze - Pirmasens und Umgebung. Egal ob Club, Hochzeit oder Event - DJ Booze hat immer die richtigen Tracks dabei.',
+    ogDescription: 'DJ Booze - Pirmasens und Umgebung. Egal ob Club, Hochzeit oder Event - DJ Booze hat immer die richtigen Tracks dabei.',
     ogImage: 'https://www.djbooze.de/_vercel/image?url=/djbooze-dark.png&w=1536&q=100',
 })
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,30 +2,39 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 
-	modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/turnstile', [
-        '@nuxtjs/google-fonts',
-        {
-            families: {
-                Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-            },
-            display: 'swap',
-        },
-    ], [
-        'nuxt-mail',
-        {
-            message: {
-                to: process.env.EMAIL_APP_EMAIL,
-            },
-            smtp: {
-                host: process.env.EMAIL_APP_HOST,
-                port: 587,
-                auth: {
-                    user: process.env.EMAIL_APP_EMAIL,
-                    pass: process.env.EMAIL_APP_PASSWORD,
-                },
-            },
-        },
-    ], '@samk-dev/nuxt-vcalendar', "@nuxt/scripts"],
+	modules: [
+		'@nuxt/ui',
+		'@nuxt/image',
+		'@nuxtjs/turnstile',
+		[
+			'@nuxtjs/google-fonts',
+			{
+				families: {
+					Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+				},
+				display: 'swap',
+			},
+		],
+		[
+			'nuxt-mail',
+			{
+				message: {
+					to: process.env.EMAIL_APP_EMAIL,
+				},
+				smtp: {
+					host: process.env.EMAIL_APP_HOST,
+					port: 587,
+					auth: {
+						user: process.env.EMAIL_APP_EMAIL,
+						pass: process.env.EMAIL_APP_PASSWORD,
+					},
+				},
+			},
+		],
+		'@samk-dev/nuxt-vcalendar',
+		'@nuxt/scripts',
+		'@nuxtjs/seo',
+	],
 
 	ui: {
 		// @ts-expect-error Somehow the typing for this is wrong
@@ -54,4 +63,11 @@ export default defineNuxtConfig({
 	},
 
 	compatibilityDate: '2024-07-06',
+	site: {
+		url: 'https://djbooze.de',
+		name: 'DJ Booze',
+		description:
+			'DJ Booze - Pirmasens und Umgebung. Egal ob Club, Hochzeit oder Event - DJ Booze hat immer die richtigen Tracks dabei.',
+		defaultLocale: 'de-DE', // not needed if you have @nuxtjs/i18n installed
+	},
 });
