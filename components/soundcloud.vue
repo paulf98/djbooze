5cb9ece36ba66e15a9613ef113e432a11371f0f0
Paul Faass <paulfaass@gmail.com>
2024-10-17 09:38:16 +0200

    fix: lazy load soundcloud widget

--- components/soundcloud.vue
+++ components/soundcloud.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay"
+        <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" loading="lazy"
             :src="`https://w.soundcloud.com/player/?url=${url}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`"></iframe>
     </div>
 </template>
